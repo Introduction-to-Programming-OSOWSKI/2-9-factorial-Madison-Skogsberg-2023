3b1f2795fd953a177fcf0ebd215fdffb61dd9f38
Madison-Skogsberg-2023 <816418@mystma.org>
2021-10-05 09:09:31 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #WRITE YOUR CODE IN THIS FILE
+#define function
+#add parameter
+def factorial(x):
+    y = 1
+    for i in range (1, x + 1):
+        y = i * y
+    return y
+#run function
+print(factorial(10))
